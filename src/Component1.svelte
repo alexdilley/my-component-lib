1e7290fc866597369ea3fb5469095112ec23d096
Alex Dilley <alex.dilley@gmail.com>
2019-09-05 09:01:49 +0100

    initial commit

---
+++ src/Component1.svelte
@@ -0,0 +1,27 @@
+<script>
+  export let items = [];
+</script>
+
+<style>
+  ul {
+    padding: 0;
+    margin: 0;
+    list-style: none;
+  }
+
+  li {
+    color: rebeccapurple;
+  }
+
+  li:nth-child(even) {
+    color: #998033;
+  }
+</style>
+
+<ul>
+  {#each items as item}
+    <li>
+      <slot item={item}>{item}</slot>
+    </li>
+  {/each}
+</ul>
